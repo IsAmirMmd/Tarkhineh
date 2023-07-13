cc173ac9a33fd449616061e3ffcacbf9dfd1c3a2
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-13 23:26:58 +0330

    making the header responive for phone and desktops...

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,30 @@
 import swiper from "./components/swiper.js";
 
-export default app;
+const profileMenu = document.querySelector(".profile__menu");
+const profileDownCaret = document.querySelector(".arrow-down-profile");
+const hamburgerMenu = document.querySelector(".header__hamburger");
+const headerItem = document.querySelector(".header__links");
+const modalBackground = document.querySelector(".modal-background");
+const closePanel = document.querySelector(".close-panel");
+
+class app {
+  constructor() {
+    profileDownCaret.addEventListener("click", () => {
+      profileMenu.classList.toggle("active");
+    });
+
+    hamburgerMenu.addEventListener("click", () => {
+      console.log(hamburgerMenu);
+      headerItem.classList.add("active");
+    });
+
+    modalBackground.addEventListener("click", () => {
+      headerItem.classList.remove("active");
+    });
+
+    closePanel.addEventListener("click", () => {
+      headerItem.classList.remove("active");
+    });
+  }
+}
+export default new app();
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,5 @@
 import app from "./app.js";
+
+addEventListener("DOMContentLoaded", () => {
+  app;
+});
